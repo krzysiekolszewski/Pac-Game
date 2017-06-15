47a4cdbb52f8b250f5572564da620e600ae60163
krzysiekolszewski <krzols95@gmail.com>
2017-06-15 12:28:07 +0200

    final changes - styling & overlay

--- js/out.js
+++ js/out.js
@@ -87,7 +87,6 @@ document.addEventListener("DOMContentLoaded", function() {
         this.x = Math.floor(Math.random() * 10);
         this.y = Math.floor(Math.random() * 10);
     }
-    //create function that starts the game
     function Game() {
         this.board = document.querySelectorAll('#board > div');
         this.pac = new Pac();
@@ -98,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function() {
         this.index = function(x, y) {
             return x + (y * 10);
         }
-
+        //function showing Pac on board & changing it's direction
         this.showPac = function() {
             this.board[this.index(this.pac.x, this.pac.y)].classList.add('pac');
 
@@ -113,22 +112,24 @@ document.addEventListener("DOMContentLoaded", function() {
             this.checkBonusCollision();
             this.checkPointCollision();
         }
-
+        // function showing strawberry on the board
         this.showPoint = function() {
             this.board[this.index(this.point.x, this.point.y)].classList.add('point');
         }
-
+        //function responsible for setting interval - how fast will Pac move on the board
         var self = this;
         this.startGame = function() {
             return this.idSetInterval = setInterval(function() {
                 self.movePac();
             }, this.time);
         }
+        //function showing bonus object worth 3 points and generating it in random time
         this.showBonus =  function() {
             return this.setTimer = setInterval(function() {
                 self.board[self.index(self.bonus.x, self.bonus.y)].classList.add('bonus');
             }, Math.floor(Math.random() * 20000)+10000);
         }
+        //function responsible for moving pac
         this.movePac = function() {
             this.hideVisiblePac();
             this.hideDirection();
@@ -143,9 +144,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             this.gameOver();
         }
+        //delete class after Pac moves, so that it won't multiply
         this.hideVisiblePac = function() {
             document.querySelector('.pac').classList.remove('pac');
         }
+        //delete class after Pac moves, so that it has correct direction
         this.hideDirection = function() {
             if (document.querySelector('.bottom')) {
                 document.querySelector('.bottom').classList.remove('bottom');
@@ -155,6 +158,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector('.left').classList.remove('left');
             }
         }
+        //event, that allows to move Pac with keyboard arrows
         this.turnPac = function(event) {
             switch (event.which) {
                 case 37:
@@ -170,6 +174,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     this.pac.direction = 'bottom';
             }
         }
+        //function checking collision with strawberry
         this.checkPointCollision = function() {
             if (this.pac.x === this.point.x && this.pac.y === this.point.y) {
                 document.querySelector('.point').classList.remove('point');
@@ -179,6 +184,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector('#score > div > strong').innerText = this.score;
             }
         }
+        //function checking collision with bonus point
         this.checkBonusCollision = function() {
             if (this.pac.x === this.bonus.x && this.pac.y === this.bonus.y) {
                 document.querySelector('.bonus').classList.remove('bonus');
@@ -188,17 +194,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.querySelector('#score > div > strong').innerText = this.score;
             }
         }
+        //function checking if a game is over (if pac has run into wall)
         this.gameOver = function() {
             if (this.pac.x < 0 || this.pac.x > 9 || this.pac.y < 0 || this.pac.y > 9) {
                 clearInterval(this.idSetInterval);
-                document.querySelector("#board").innerText = ("GAME OVER");
-                document.querySelector("#board").style.fontSize = ("40px");
-                document.querySelector("#board").style.textAlign = ("center");
+                //after the game is over, show overlay with score and start button
+                document.querySelector('.invisible').style.display = ('inline-block');
+                document.querySelector('#final_score').innerText = this.score;
             } else {
                 this.showPac();
             }
         }
     }
+    //calling game
     var game = new Game();
     game.startGame();
     game.showPac();
